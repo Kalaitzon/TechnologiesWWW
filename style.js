0cfd95d49255eb1fe066fcd86946f82252f83025
Ioannis Kalaitzidis <120419392+Kalaitzon@users.noreply.github.com>
2023-01-15 16:05:05 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,204 @@
+let a = document.querySelector("#button1")
+a.addEventListener('click', () => {
+    document.querySelector('#message').style.visibility = 'visible';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#danbrown').style.visibility = 'hidden';
+    document.querySelector('#bookcovers').style.visibility = 'hidden';
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+    document.querySelector('#login').style.visibility = 'hidden';
+    document.querySelector('#logout').style.visibility = 'hidden';
+    document.querySelector('#linkmanagement').style.visibility = 'hidden';
+    document.querySelector('#projectmanagement').style.visibility = 'hidden';
+});
+
+let b = document.querySelector("#button6")
+b.addEventListener('click', () => {
+    document.querySelector('#whoishe').style.visibility = 'visible';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+});
+
+let c = document.querySelector("#button7")
+c.addEventListener('click', () => {
+    document.querySelector('#earlylife').style.visibility = 'visible';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+});
+
+let d = document.querySelector("#button8")
+d.addEventListener('click', () => {
+    document.querySelector('#career').style.visibility = 'visible';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+});
+//-------------------------------------------------------------------------------------------------------
+let e = document.querySelector("#button2")
+e.addEventListener('click', () => {
+    document.querySelector('#message1').style.visibility = 'visible';
+    document.querySelector('#message').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+    document.querySelector('#login').style.visibility = 'hidden';
+    document.querySelector('#logout').style.visibility = 'hidden';
+    document.querySelector('#linkmanagement').style.visibility = 'hidden';
+    document.querySelector('#projectmanagement').style.visibility = 'hidden';
+});
+
+let f = document.querySelector("#button9")
+f.addEventListener('click', () => {
+    document.querySelector('#danbrown').style.visibility = 'visible';
+    document.querySelector('#bookcovers').style.visibility = 'hidden';
+});
+
+let g = document.querySelector("#button10")
+g.addEventListener('click', () => {
+    document.querySelector('#bookcovers').style.visibility = 'visible';
+    document.querySelector('#danbrown').style.visibility = 'hidden';
+});
+//----------------------------------------------------------------------------------------------
+let h = document.querySelector("#button3")
+h.addEventListener('click', () => {
+    document.querySelector('#message2').style.visibility = 'visible';
+    document.querySelector('#message').style.visibility = 'hidden';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+    document.querySelector('#danbrown').style.visibility = 'hidden';
+    document.querySelector('#bookcovers').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+    document.querySelector('#login').style.visibility = 'hidden';
+    document.querySelector('#logout').style.visibility = 'hidden';
+    document.querySelector('#linkmanagement').style.visibility = 'hidden';
+    document.querySelector('#projectmanagement').style.visibility = 'hidden';
+});
+
+let i = document.querySelector("#button11")
+i.addEventListener('click', () => {
+    document.querySelector('#robertlangdonseries').style.visibility = 'visible';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+});
+
+let j = document.querySelector("#button12")
+j.addEventListener('click', () => {
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'visible';
+});
+//----------------------------------------------------------------------------------------------------------
+let m = document.querySelector("#button4")
+m.addEventListener('click', () => {
+    document.querySelector('#message3').style.visibility = 'visible';
+    document.querySelector('#message').style.visibility = 'hidden';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message4').style.visibility = 'hidden';
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+    document.querySelector('#danbrown').style.visibility = 'hidden';
+    document.querySelector('#bookcovers').style.visibility = 'hidden';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+    document.querySelector('#login').style.visibility = 'hidden';
+    document.querySelector('#logout').style.visibility = 'hidden';
+    document.querySelector('#linkmanagement').style.visibility = 'hidden';
+    document.querySelector('#projectmanagement').style.visibility = 'hidden';
+});
+
+let n = document.querySelector("#button13")
+n.addEventListener('click', () => {
+    document.querySelector('#bibliography').style.visibility = 'visible';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+});
+
+let o = document.querySelector("#button14")
+o.addEventListener('click', () => {
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'visible';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+});
+
+let p = document.querySelector("#button15")
+p.addEventListener('click', () => {
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'visible';
+    document.querySelector('#support').style.visibility = 'hidden';
+});
+
+let q = document.querySelector("#button16")
+q.addEventListener('click', () => {
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'visible';
+});
+//-------------------------------------------------------------------------------------
+let r = document.querySelector("#button5")
+r.addEventListener('click', () => {
+    document.querySelector('#message4').style.visibility = 'visible';
+    document.querySelector('#message').style.visibility = 'hidden';
+    document.querySelector('#message1').style.visibility = 'hidden';
+    document.querySelector('#message2').style.visibility = 'hidden';
+    document.querySelector('#message3').style.visibility = 'hidden';
+    document.querySelector('#standalonenovels').style.visibility = 'hidden';
+    document.querySelector('#robertlangdonseries').style.visibility = 'hidden';
+    document.querySelector('#danbrown').style.visibility = 'hidden';
+    document.querySelector('#bookcovers').style.visibility = 'hidden';
+    document.querySelector('#earlylife').style.visibility = 'hidden';
+    document.querySelector('#whoishe').style.visibility = 'hidden';
+    document.querySelector('#career').style.visibility = 'hidden';
+    document.querySelector('#bibliography').style.visibility = 'hidden';
+    document.querySelector('#interviews').style.visibility = 'hidden';
+    document.querySelector('#follow').style.visibility = 'hidden';
+    document.querySelector('#support').style.visibility = 'hidden';
+});
+
+let s = document.querySelector("#button17")
+s.addEventListener('click', () => {
+    document.querySelector('#login').style.visibility = 'visible';
+});
+
+let t = document.querySelector("#button17")
+t.addEventListener('click', () => {
+    document.querySelector('#logout').style.visibility = 'visible';
+
+});
+
+let u = document.querySelector("#button17")
+u.addEventListener('click', () => {
+    document.querySelector('#projectmanagement').style.visibility = 'visible';
+});
+
+let v = document.querySelector("#button17")
+v.addEventListener('click', () => {
+    document.querySelector('#linkmanagement').style.visibility = 'visible';
+});
